ac824b2e620d6150ac71a64b99c14b04763e2249
Kevin Griffin <xgriffin.kevin@gmail.com>
2010-07-16 21:52:10 -0500

    Move notification out of reconnect code

--- source/wifi.rb
+++ source/wifi.rb
@@ -6,8 +6,7 @@ class WiFiConnectionStatus
     @growler  = Growl.new 'localhost', 'wifi', ['offline']
   end
 
-  def is_down
-    @growler.notify 'offline', 'The internet is gone again', 'The Wifi has left the building' if @online
+  def reconnect
     %x[osascript airport_off.scpt]
     sleep(3)      
     %x[osascript airport_on.scpt] 
@@ -16,7 +15,8 @@ class WiFiConnectionStatus
   def check_connection
     result = %x(ping -W2 -c3 google.com 2>&1)
     if result["100.0% packet loss"] || result["Unknown"]
-      is_down
+      @growler.notify 'offline', 'The internet is gone again', 'The Wifi has left the building' if @online
+      reconnect
       @online = false
     else
       @growler.notify 'offline', 'The internet is back', 'Enjoy it while it lasts' unless @online
