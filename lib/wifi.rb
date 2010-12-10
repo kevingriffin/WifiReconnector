43b09cd5aab5330079fc3828c574fb1489fb8177
Michael Baker <michaeltbaker@gmail.com>
2010-12-10 16:05:59 -0500

    Add disconnect count and snarks

--- lib/wifi.rb
+++ lib/wifi.rb
@@ -3,23 +3,41 @@ require 'ruby-growl'
 class WiFiConnectionStatus
   def initialize
     @online   = true
-    @growler  = Growl.new 'localhost', 'wifi', ['offline']
-  end
+    @growler  = Growl.new 'localhost', 'wifi', ['offline', 'online']
+    @count    = 0
 
+    File.open "snarks.txt" do |file|
+      @snarks = file.readlines 
+    end
+  end
+  
   def reconnect
     %x[osascript #{File.dirname(__FILE__)}/wifi/airport_off.scpt]
     sleep(3)      
     %x[osascript #{File.dirname(__FILE__)}/wifi/airport_on.scpt] 
   end
 
+  def snark
+    @online ? @snarks.shuffle.first.strip + "\n" : ''
+  end
+  
+  def disconnection_count
+    @online ? "\nDisconnection count: #{@count}" : ''
+  end
+  
+  def growl( arguments )
+    @growler.notify arguments[:name] , arguments[:title] || '' , snark + (arguments[:body] || '') + disconnection_count
+  end
+  
   def check_connection
     result = %x(ping -W2 -c3 google.com 2>&1)
     if result["100.0% packet loss"] || result["Unknown"]
-      @growler.notify 'offline', 'The internet is gone again', 'The Wifi has left the building' if @online
+      @count += 1 if @online
+      growl( :name => 'offline', :title => 'The internet is gone again', :body => "You've lost your Wifi connection again." ) if @online
       reconnect
       @online = false
     else
-      @growler.notify 'offline', 'The internet is back', 'Enjoy it while it lasts' unless @online
+      growl( :name => 'online', :title => 'The internet is back', :body => 'Enjoy it while it lasts' ) unless @online
       @online = true
     end
   end
